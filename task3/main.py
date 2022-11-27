3a102d3a80c9f3ea5ceb40a14489f9f6a7f6d59c
Dany <86149558+QBoff@users.noreply.github.com>
2022-11-27 18:21:52 +0300

    task3 + gitignore

---
+++ task3/main.py
@@ -0,0 +1,65 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from os.path import join
+
+import random
+
+
+class MyWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        uic.loadUi(join("task3", "main.ui"), self)
+
+        self.btn.clicked.connect(self.doPaint)
+        self.paint = False
+
+    def drawEllipse(self, qp):
+        qp.setBrush(self.randColor())
+        width, height = QMainWindow.width(self), QMainWindow.height(self)
+
+        othercorsx, othercorsy = set(), set()
+
+        radius = random.randrange(50, 200)
+        cors = random.randrange(
+            0, width - 2 * radius), random.randrange(0, height - 2 * radius)
+        othercorsx = set(range(cors[0], cors[0] + radius + 1))
+        othercorsy = set(range(cors[1], cors[1] + radius + 1))
+        qp.drawEllipse(cors[0], cors[1], radius, radius)
+
+        radius = random.randrange(50, 200)
+        cors = random.randrange(
+            0, width - 2 * radius), random.randrange(0, height - 2 * radius)
+
+        while set(range(cors[0], cors[0] + radius + 1)) & othercorsx != set() and \
+                set(range(cors[1], cors[1] + radius + 1)) & othercorsy != set():
+            cors = random.randrange(
+                0, width - 2 * radius), random.randrange(0, height - 2 * radius)
+
+        qp.drawEllipse(cors[0], cors[1], radius, radius)
+
+    def randColor(self):
+        r, g, b = random.randrange(0, 255), random.randrange(
+            0, 255), random.randrange(0, 255)
+        return QColor(r, g, b)
+
+    def paintEvent(self, event):
+        if self.paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawEllipse(qp)
+            qp.end()
+            self.paint = False
+
+    def doPaint(self):
+        self.paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWindow()
+    ex.show()
+    sys.exit(app.exec())
